d91ac2b8520e3c799fc89618f156e3b10b46f160
Henryck <henryck.leandro@gmail.com>
2019-10-02 20:11:58 -0300

    Adicionado o comeco do backend

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const routes = require("./routes");
+const PORT = 4000;
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
