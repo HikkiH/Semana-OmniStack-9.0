971e639c6c96fc64cb80270bc49412332181cfe3
Henryck <henryck.leandro@gmail.com>
2019-10-05 19:02:16 -0300

    cors adicionado ao backend, rotas adicionada ao frontend e pagina de login implementada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes");
 const dbConfig = require("./config/database");
@@ -13,6 +14,7 @@ mongoose.connect(dbConfig.uri, {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
